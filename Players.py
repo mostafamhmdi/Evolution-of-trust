18a918591fd784377ee7a016b8f446e292049ff9
mostafamhmdi <mohammadimostafa858@gmail.com>
2024-03-25 23:47:45 +0330

    Add more players

--- Players.py
+++ Players.py
@@ -7,30 +7,75 @@ class Player(ABC):
         self.money=initial_money
 
     @abstractmethod
-    def perform_action(self):
+    def perform_action(self, opponent_last_action):
         pass
 
 class Generous(Player):
     def __init__(self, name):
         super().__init__(name)
 
-    def perform_action(self):
+    def perform_action(self, opponent_last_action):
         return "Cooperate"
 
 class Selfish(Player):
     def __init__(self, name):
         super().__init__(name)
 
-    def perform_action(self):
+    def perform_action(self, opponent_last_action):
         return "Betray"
+
 
 class RandomPlayer(Player):
     def __init__(self, name):
         super().__init__(name)
 
-    def perform_action(self):
+    def perform_action(self, opponent_last_action):
         actions = ["Cooperate", "Betray"]
         return random.choice(actions)
+    
+
+class CopyCat(Player):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def perform_action(self, opponent_last_action):
+        return opponent_last_action
 
 
+class Grudger(Player):
+    def __init__(self, name):
+        super().__init__(name)
 
+    
+    def perform_action(self, opponent_last_action):
+        actions=[]
+        actions.append(opponent_last_action)
+        if "Betray" in actions:
+            return "Betray"
+        else:
+            return "Cooperate"
+            
+
+class Detective(Player):
+    def __init__(self, name):
+        super().__init__(name)
+
+    
+    def perform_action(self, opponent_last_action):
+        actions=[]
+        i=1
+        actions.append(opponent_last_action)
+        if i==1:
+            return "Cooperate"
+        elif i==2:
+            return "Betray"
+        elif i==3:
+            return "Cooperate"
+        elif i==4:
+            return "Cooperate"
+
+        elif i > 4:
+            if "Betray" in actions:
+                return opponent_last_action
+            else:
+                return "Cooperate"
--- TrustEvolution.py
+++ TrustEvolution.py
@@ -1,11 +1,15 @@
 import random
-from Players import Player,Generous,Selfish,RandomPlayer
+from Players import Player,Generous,Selfish,RandomPlayer,CopyCat,Grudger,Detective
 class Game:
     def __init__(self):
         self.players = [
             Generous("Generous Player"),
             Selfish("Selfish Player"),
-            RandomPlayer("Random Player")
+            RandomPlayer("Random Player"),
+            CopyCat("CopyCat Player"),
+            Grudger("Grudger Player"),
+            Detective("Detective Player")
+
         ]
         self.num_rounds = 7
 
@@ -18,13 +22,20 @@ class Game:
                 player1 = self.players[i]
                 player2 = self.players[j]
 
+
+                player1_last_action = "Cooperate"
+                player2_last_action = "Cooperate"
+
                 print(f"{player1.name} vs {player2.name}:")
                 player1.money = player1.money
                 player2.money = player2.money
                 print(f"{player1.name} :{player1.money} vs {player2.name}:{player2.money} :")
                 for _ in range(self.num_rounds):
-                    action1 = player1.perform_action()
-                    action2 = player2.perform_action()
+                    # action1 = player1.perform_action()
+                    # action2 = player2.perform_action()
+
+                    action1 = player1.perform_action(player2_last_action)
+                    action2 = player2.perform_action(player1_last_action)
 
                     if action1 == "Cooperate" and action2 == "Cooperate":
                         player1.money += 2
@@ -42,6 +53,8 @@ class Game:
                         player1.money += 0
                         player2.money += 0
 
+                    player1_last_action = action1  
+                    player2_last_action = action2
 
                 print(f"{player1.name} final money: {player1.money}")
                 print(f"{player2.name} final money: {player2.money}")
@@ -49,10 +62,10 @@ class Game:
         print("Game over!")
 
 
-
 def main():
     game = Game()
     game.start()
+    
 
 if __name__ == "__main__":
     main()
